5140922351d8ca3baacade4715e25e38f54121ec
mossac <mossac@rose-hulman.edu>
2019-01-31 20:25:59 -0500

    DONE

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,47 +3,73 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Aidan Moss.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
+def decide(entry_box):
+    h =entry_box.get()
+    if h is 'ok':
+        print('Hello')
+        return
+    else:
+        print('Goodbye')
+    return
+def run(entry_box,n):
+    string = entry_box.get()
+    for k in range(n):
+        print(string)
 
 
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DONE: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    root = tkinter.Tk()
+
+
 
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DONE: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
+    frame1 = ttk.Frame(root, padding = 15)
+    frame1.grid()
 
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DONE: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
+    forward_button= ttk.Button(frame1, text = 'Print')
+    forward_button.grid()
 
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DONE: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
+    forward_button['command']=(lambda : print('hello'))
 
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DONE: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+    text_button = ttk.Button(frame1, text= 'Enter')
+    entry_box = ttk.Entry(frame1)
+    entry_box.grid()
+    text_button.grid()
+
+    text_button['command'] = (lambda: decide(entry_box))
 
     # -------------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -57,6 +83,14 @@ def main():
     #    that is a "user error" -- do NOT deal with that.
     #
     # -------------------------------------------------------------------------
+    text_button2 = ttk.Button(frame1, text='RUN')
+    entry_box2 = ttk.Entry(frame1)
+    entry_box2.grid()
+    text_button2.grid()
+    text_button2['command'] = (lambda: run(entry_box,int(entry_box2.get())))
+
+
+
     ####################################################################
     # HINT:
     #   You will need to obtain the INTEGER from the STRING
@@ -67,11 +101,17 @@ def main():
     ####################################################################
 
     # -------------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DONE: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
+    root.mainloop()
+
 
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
 main()
+
+
+
+
